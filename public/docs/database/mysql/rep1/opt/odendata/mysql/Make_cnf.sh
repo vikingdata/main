a06656c452bbf78e541ae33f2c056bdd610fc4e6
m@menprojects.com <git@menprojects.com>
2022-01-09 05:40:00 -0800

    auto

--- public/docs/database/mysql/rep1/opt/odendata/mysql/Make_cnf.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Make_cnf.sh
@@ -3,9 +3,9 @@
 
 for i in 1 2 3 4 5 6; do
 
-    mkdir -p ../data/mysql/mysql$i/binlogs  
+    mkdir -p ../../../data/mysql/mysql$i/binlogs  
     echo $f  
-    f=../data/mysql/mysql$i/my.cnf
+    f=../../../data/mysql/mysql$i/my.cnf
     cp -f my_template.cnf $f  
     sed -i "s/__NO__/$i/g" $f
     
