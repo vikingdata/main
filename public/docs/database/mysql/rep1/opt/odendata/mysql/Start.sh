b629df49ab814991dea7ff8ba472583b3990e2cf
m@menprojects.com <git@menprojects.com>
2022-01-09 14:52:21 -0800

    auto

---
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Start.sh
@@ -0,0 +1,22 @@
+
+# For this version of MariaDB, you must remove aria_control default file.
+# It will atempt to use that, but if it is not there, it will use the one
+# defined in my.cnf.
+# When I look at the buglist, it might have already been addressed.
+
+
+rm -f /var/lib/mysql/*
+
+# If this doesn't start, you may have to restart the host.
+# In windoze, I ran it under Docker, and could only
+# get it to work after a restart probably because I used up to
+# much memory doing other stuff and I think the Docker service needed
+# an upgrade --- I had to manually start the service. 
+
+for i in 1 2 3 4 5 6; do
+    echo "starting mysql $i"   
+    mysqld --defaults-file=/data/mysql/mysql$i/my.cnf &
+    sleep 5
+done    
+
+ps auxww | grep mysqld
