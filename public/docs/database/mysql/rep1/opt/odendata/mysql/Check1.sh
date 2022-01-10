ee8f7551e6fa28b47deaf178c6e8730edd134a88
m@menprojects.com <git@menprojects.com>
2022-01-10 01:15:05 -0800

    auto

---
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Check1.sh
@@ -0,0 +1,18 @@
+
+sql="insert into test_od.hosts values ('1',now())"
+mysql -v -u root -e "$sql" -S /tmp/mysql1.sock
+
+sql="insert into test_od.hosts values ('1',now())"
+mysql -v -u root -e "$sql" -S /tmp/mysql1.sock
+
+for i in 1 2 3 4 5 6; do
+  sql="select @@server_id, host, date_insert from test_od.hosts where host = '1' 
+  order by date_insert desc limit 1"
+
+  sleep 2  
+  for i in 1 2 3 4 5 6; do
+  echo "try i, server $i'"
+  mysql -v -u root -e "$sql" -S /tmp/mysql$i.sock
+  done
+done
+	 
--- public/docs/database/mysql/rep1/opt/odendata/mysql/Replication.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Replication.sh
@@ -1,9 +1,12 @@
-
 killall mysqld
 sleep 5
 
-rm -vf /data/mysql/mysql*/binlogs/*
+echo "removing binlogs"
+rm -vf /var/log/mysql/mysql*/binlogs/*
+rm -vf /var/log/mysql/mysql*/relay/*
 
+rm -vf /data/mysql/mysql*/data/mysql-bin*
+rm -vf /data/mysql/mysql*/data/*relay*
 
 for i in 1 2 3 4 5 6; do
   echo "starting mysql $i"
@@ -15,15 +18,20 @@ done
 for i in 1 2 3 4 5 6; do
   echo "flushing logs mysql $i"
   mysql -v -u root -e "flush logs" -S /tmp/mysql$i.sock
+  mysql -v -u root -e "show master logs" -S /tmp/mysql$i.sock
 done
 
+for i in 1 2 3 4 5 6; do
+  echo "start slave mysql $i"
+  mysql -v -u root -e "stop slave\G" -S /tmp/mysql$i.sock
+done
 
 sql_slave1="
 change master to MASTER_HOST='127.0.0.1',
   MASTER_USER='rep',
   MASTER_PASSWORD='rep',
   MASTER_LOG_FILE='mysql-bin.000001',
-  MASTER_LOG_POS=365,
+  MASTER_LOG_POS=375,
   master_port=3301;
 "
 
@@ -32,12 +40,11 @@ change master to MASTER_HOST='127.0.0.1',
   MASTER_USER='rep',
   MASTER_PASSWORD='rep',
   MASTER_LOG_FILE='mysql-bin.000001',
-  MASTER_LOG_POS=365,
+  MASTER_LOG_POS=375,
   master_port=3302;
 "
 
 sql_delay="CHANGE MASTER TO MASTER_DELAY = 10;"
-
 #SHOW VARIABLES LIKE 'rpl_semi_sync%';
 
 # m-m
@@ -45,16 +52,40 @@ mysql -v -u root -e "$sql_slave2" -S /tmp/mysql1.sock
 mysql -v -u root -e "$sql_slave1" -S /tmp/mysql2.sock
 
 # slaves off first master
-
 mysql -v -u root -e "$sql_slave1" -S /tmp/mysql3.sock
 mysql -v -u root -e "$sql_slave1" -S /tmp/mysql5.sock
-
 # slave off 2nd master
 
 mysql -v -u root -e "$sql_slave2" -S /tmp/mysql4.sock
 mysql -v -u root -e "$sql_slave2" -S /tmp/mysql6.sock
 
 # 10 second dlay slaves
-
 mysql -v -u root -e "$sql_delay" -S /tmp/mysql5.sock
 mysql -v -u root -e "$sql_delay" -S /tmp/mysql6.sock
+
+for i in 1 2 3 4 5 6; do
+  echo "show slave mysql $i"
+  mysql -v -u root -e "show slave status\G" -S /tmp/mysql$i.sock \
+  | egrep -i 'running|master_host|master_port';
+done
+
+
+for i in 1 2 3 4 5 6; do
+  echo "start slave mysql $i"
+  mysql -v -u root -e "start slave\G" -S /tmp/mysql$i.sock
+  sleep 1
+done
+
+sleep 10
+
+for i in 1 2 3 4 5 6; do
+  echo "show slave mysql $i"
+  mysql -v -u root -e "show slave status\G" -S /tmp/mysql$i.sock \
+	| egrep -i 'running|master_host|master_port';
+done
+
+
+# mysql -v -u root -e "show slave status\G" -S /tmp/mysql1.sock
+
+
+
