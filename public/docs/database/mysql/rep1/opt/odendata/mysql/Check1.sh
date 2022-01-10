1e149f98cffa5ba56217fa9fbf452f95ba706de2
m@menprojects.com <git@menprojects.com>
2022-01-10 01:22:31 -0800

    auto

--- public/docs/database/mysql/rep1/opt/odendata/mysql/Check1.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Check1.sh
@@ -2,14 +2,14 @@
 sql="insert into test_od.hosts values ('1',now())"
 mysql -v -u root -e "$sql" -S /tmp/mysql1.sock
 
-for i in 1 2 3 4 5 6; do
-  sql="select @@server_id, host, date_insert from test_od.hosts where host = '1' 
-  order by date_insert desc limit 1"
+sql="select @@server_id, host, date_insert from test_od.hosts where host = '1'             
+       order by date_insert desc limit 1"
+  
+for i in 1 2 3 4 5 6 7 8 9 10 11; do
 
   sleep 2  
   for j in 1 2 3 4 5 6; do
-    echo "try $j, server $i'"
-    mysql -v -u root -e "$sql" -S /tmp/mysql$i.sock
+    echo "try $i, server $j'", `mysql -v -u root -e "$sql" -S /tmp/mysql$j.sock`
   done
 done
 	 
