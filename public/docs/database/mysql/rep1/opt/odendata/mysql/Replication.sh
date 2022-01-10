88e29016171d7361b0968973a424d9af7cfaf586
m@menprojects.com <git@menprojects.com>
2022-01-09 23:45:20 -0800

    auto

--- public/docs/database/mysql/rep1/opt/odendata/mysql/Replication.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Replication.sh
@@ -1,4 +1,22 @@
 
+killall mysqld
+sleep 5
+
+rm -vf /data/mysql/mysql*/binlogs/*
+
+
+for i in 1 2 3 4 5 6; do
+  echo "starting mysql $i"
+  echo "mysqld --defaults-file=/data/mysql/mysql$i/my.cnf &"
+  mysqld --defaults-file=/data/mysql/mysql$i/my.cnf &
+  sleep 5
+done
+
+for i in 1 2 3 4 5 6; do
+  echo "flushing logs mysql $i"
+  mysql -v -u root -e "flush logs" -S /tmp/mysql$i.sock
+done
+
 
 sql_slave1="
 change master to MASTER_HOST='127.0.0.1',
@@ -22,10 +40,21 @@ sql_delay="CHANGE MASTER TO MASTER_DELAY = 10;"
 
 #SHOW VARIABLES LIKE 'rpl_semi_sync%';
 
+# m-m
 mysql -v -u root -e "$sql_slave2" -S /tmp/mysql1.sock
+mysql -v -u root -e "$sql_slave1" -S /tmp/mysql2.sock
+
+# slaves off first master
+
+mysql -v -u root -e "$sql_slave1" -S /tmp/mysql3.sock
+mysql -v -u root -e "$sql_slave1" -S /tmp/mysql5.sock
+
+# slave off 2nd master
+
 mysql -v -u root -e "$sql_slave2" -S /tmp/mysql4.sock
 mysql -v -u root -e "$sql_slave2" -S /tmp/mysql6.sock
 
-mysql -v -u root -e "$sql_slave1" -S /tmp/mysql2.sock
-mysql -v -u root -e "$sql_slave1" -S /tmp/mysql3.sock
-mysql -v -u root -e "$sql_slave1" -S /tmp/mysql5.sock
+# 10 second dlay slaves
+
+mysql -v -u root -e "$sql_delay" -S /tmp/mysql5.sock
+mysql -v -u root -e "$sql_delay" -S /tmp/mysql6.sock
