f23a1794f76e9560fc87af0378947b6fb2cbba2c
m@menprojects.com <git@menprojects.com>
2022-01-09 18:06:42 -0800

    auto

--- public/docs/database/mysql/rep1/opt/odendata/mysql/Accounts.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Accounts.sh
@@ -1,12 +1,19 @@
 
 
-echo sql=" CREATE USER 'rep'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'rep';  FLUSH PRIVILEGES;"
+sql=" CREATE USER 'rep'@'localhost' IDENTIFIED BY 'rep'; FLUSH PRIVILEGES;"
 
+sql2="GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.*
+TO 'rep'@'localhost'"
 
 # We assume root's password is set in ~/.mysql.my.cnf or its blank.
 # If not do it.
 
 for i in 1 2 3 4 5 6; do
-    echo "Setting up rep in mysql $i" 
+
+    echo "Setting up rep in mysql $i"
     mysql -v -u root -e "$sql" -S /tmp/mysql$i.sock
+    mysql -vv -u root -e "$sql2" -S /tmp/mysql$i.sock
+    mysql -v -u root -e "show grants for rep@localhost" -S /tmp/mysql$i.sock
 done
+
+
--- public/docs/database/mysql/rep1/opt/odendata/mysql/Start.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Start.sh
@@ -1,9 +1,25 @@
 
+#rm -f /var/log/mysql/mysql*/*.log
+
+#export web=http://odendata.com/docs/database/mysql/rep1/data/mysql
+
+#for i in 1 2 3 4 5 6; do
+# dest=/data/mysql/mysql$i/my.cnf
+# webcnf=$web/mysql$i/my.cnf
+# echo " wget -vo $dest $webcnf"
+# wget -O $dest $webcnf
+#done
 
 for i in 1 2 3 4 5 6; do
-    echo "starting mysql $i"   
-    mysqld --defaults-file=/data/mysql/mysql$i/my.cnf &
-    sleep 5
-done    
+  echo "starting mysql $i"
+  echo "mysqld --defaults-file=/data/mysql/mysql$i/my.cnf &"
+  mysqld --defaults-file=/data/mysql/mysql$i/my.cnf &
+  sleep 5
+done
+
+#cat /var/log/mysql/mysql1/error.log
 
 ps auxww | grep mysqld
+#ls -al /var/lib/mysql/
+
+
