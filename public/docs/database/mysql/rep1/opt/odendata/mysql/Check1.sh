1875c01a30e080dc91b520328c51c45a2b08df99
m@menprojects.com <git@menprojects.com>
2022-01-10 01:29:10 -0800

    auto

--- public/docs/database/mysql/rep1/opt/odendata/mysql/Check1.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Check1.sh
@@ -4,12 +4,19 @@ mysql -v -u root -e "$sql" -S /tmp/mysql1.sock
 
 sql="select @@server_id, host, date_insert from test_od.hosts where host = '1'             
        order by date_insert desc limit 1"
-  
-for i in 1 2 3 4 5 6 7 8 9 10 11; do
 
-  sleep 2  
-  for j in 1 2 3 4 5 6; do
-    echo "try $i, server $j'", `mysql -v -u root -e "$sql" -S /tmp/mysql$j.sock`
+for i in 1 2 3 5; do
+     echo "server $j", `mysql -u root -e "$sql" -S /tmp/mysql$1.sock`
+done
+echo "The above lines should all be the same."
+echo ""
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12; do
+
+  sleep 1
+  for j in 1 5 6; do
+      echo "second $i, server $j'", `mysql -u root -e "$sql" -S /tmp/mysql$j.sock`
   done
 done
+echo "You should have seen a time change after 10 seconds on server 5 and 6."
 	 
