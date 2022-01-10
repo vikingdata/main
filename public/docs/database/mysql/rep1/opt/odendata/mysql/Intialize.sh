70d5936a1e0b3b9eca00f6b891b04f3564b65808
m@menprojects.com <git@menprojects.com>
2022-01-09 17:39:29 -0800

    auto

--- public/docs/database/mysql/rep1/opt/odendata/mysql/Intialize.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Intialize.sh
@@ -2,23 +2,23 @@
 chown -R mysql.mysql /var/log/mysql
 chown -R mysql.mysql /data/mysql
 
-mysql_install_db --user=mysql --datadir=/data/mysql/mysql1 \
+mysql_install_db --user=mysql --datadir=/data/mysql/mysql1/data \
  --defaults-file=/data/mysql/mysql1/my.cnf
 
 
-mysql_install_db --user=mysql --datadir=/data/mysql/mysql2 \
+mysql_install_db --user=mysql --datadir=/data/mysql/mysql2/data \
 		 --defaults-file=/data/mysql/mysql3/my.cnf
 
-mysql_install_db --user=mysql --datadir=/data/mysql/mysql3 \
+mysql_install_db --user=mysql --datadir=/data/mysql/mysql3/data \
 		 --defaults-file=/data/mysql/mysql3/my.cnf
 
-mysql_install_db --user=mysql --datadir=/data/mysql/mysql4 \
+mysql_install_db --user=mysql --datadir=/data/mysql/mysql4/data \
 		 --defaults-file=/data/mysql/mysql4/my.cnf
 
-mysql_install_db --user=mysql --datadir=/data/mysql/mysql5 \
+mysql_install_db --user=mysql --datadir=/data/mysql/mysql5/data \
 		 --defaults-file=/data/mysql/mysql5/my.cnf
 
-mysql_install_db --user=mysql --datadir=/data/mysql/mysql6 \
+mysql_install_db --user=mysql --datadir=/data/mysql/mysql6/data \
 		 --defaults-file=/data/mysql/mysql6/my.cnf
 
 
