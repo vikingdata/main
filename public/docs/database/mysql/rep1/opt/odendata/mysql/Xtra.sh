2efe342706ce21617234ef32185d26b3e8291aff
m@menprojects.com <git@menprojects.com>
2022-01-12 04:00:43 -0800

    auto

--- public/docs/database/mysql/rep1/opt/odendata/mysql/Xtra.sh
+++ public/docs/database/mysql/rep1/opt/odendata/mysql/Xtra.sh
@@ -1,14 +1,11 @@
 # This is mariabdb, so we have to use mariadb-backup
-
 apt-get install mariadb-backup
 
 # We can use xtrrabackup or innobackupex
 # If xtrabackup, you should also backup the mysql database.
-
 mkdir -p /backups/mysql/mysql1
 
 d=`date +%y%m%d_%H%M%S`
-
 mariabackup --defaults-file=/data/mysql/mysql1/my.cnf \
            --datadir=/data/mysql/mysql1/data \
 	   --user=root --compress --backup \
@@ -16,7 +13,6 @@ mariabackup --defaults-file=/data/mysql/mysql1/my.cnf \
 	   --target-dir=/backups/mysql/mysql1/mysql1_$d \
 	   --socket=/tmp/mysql1.sock \
 	   --compress
-				      
 
 exclude="--exclude=gtid*,innodb*,transaction*"
 S=/data/mysql/mysql1/data/mysql
